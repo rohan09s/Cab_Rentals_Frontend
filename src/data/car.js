726e66c2a5d11b13460cbed18a133c2fee3608b4
rohan09s <rohansabnis9@gmail.com>
2026-04-28 17:29:55 +0530

    Added updated files

---
+++ src/data/car.js
@@ -0,0 +1,42 @@
+export const cars = [
+  {
+    category: "Hatchback",
+    models: ["WagonR", "Celerio"],
+    image: "https://res.cloudinary.com/dt0mm3yj1/image/upload/q_auto/f_auto/v1776594356/WhatsApp_Image_2026-04-19_at_3.48.48_PM_icyqf9.jpg",
+    rate: 12,
+    seats: "4 Seater",
+    type: "hatchback",
+    inclusions: ["Fuel", "Driver", "Car Fare"],
+    exclusions: ["Toll", "Parking", "State Tax", "Entry Fees"]
+  },
+  {
+    category: "Sedan",
+    models: ["Dzire", "Aura", "Xcent"],
+    image: "https://res.cloudinary.com/dt0mm3yj1/image/upload/q_auto/f_auto/v1776593792/aura_kaiqg7.webp",
+    rate: 13,
+    seats: "4 Seater",
+    type: "sedan",
+    inclusions: ["Fuel", "Driver", "Car Fare"],
+    exclusions: ["Toll", "Parking", "State Tax", "Entry Fees"]
+  },
+  {
+    category: "Eco 6 Seater",
+    models: ["Ertiga"],
+    image: "https://res.cloudinary.com/dt0mm3yj1/image/upload/q_auto/f_auto/v1776595474/Ertiga_1_pczowk.jpg",
+    rate: 17,
+    seats: "6 Seater",
+    type: "eco",
+    inclusions: ["Fuel", "Driver", "Car Fare"],
+    exclusions: ["Toll", "Parking", "State Tax", "Entry Fees"]
+  },
+  {
+    category: "Premium 7 Seater",
+    models: ["Innova Crysta"],
+    image: "https://res.cloudinary.com/dt0mm3yj1/image/upload/q_auto/f_auto/v1776594461/innova-crysta-9-240_qojbd1.jpg",
+    rate: 17,
+    seats: "7 Seater",
+    type: "premium",
+    inclusions: ["Fuel", "Driver", "Car Fare"],
+    exclusions: ["Toll", "Parking", "State Tax", "Entry Fees"]
+  }
+];
