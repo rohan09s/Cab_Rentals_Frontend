9d33fa599a1bff7d7624ba311131e31b20d87967
rohan09s <rohansabnis9@gmail.com>
2026-05-13 00:16:27 +0530

    Add files via upload

---
+++ src/data/routes.js
@@ -0,0 +1,37 @@
+export const routesMap = {
+  Pune: [
+    "Chhatrapati Sambhajinagar",
+    "Kolhapur",
+    "Nashik",
+    "Shirdi",
+    "Karad",
+    "Mumbai",
+    "Ahmednagar",
+    "Mahabaleshwar",
+    "Lonavala"
+  ],
+
+  Mumbai: [
+    "Chhatrapati Sambhajinagar",
+    "Kolhapur",
+    "Nashik",
+    "Shirdi",
+    "Karad",
+    "Pune",
+    "Ahmednagar",
+    "Mahabaleshwar",
+    "Lonavala"
+  ],
+
+  Lonavala: [
+    "Chhatrapati Sambhajinagar",
+    "Kolhapur",
+    "Nashik",
+    "Shirdi",
+    "Karad",
+    "Pune",
+    "Mumbai",
+    "Ahmednagar",
+    "Mahabaleshwar"
+  ]
+};
